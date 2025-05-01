9f524fb1b1a252a09c7298eb9730479bfba72bcc
Jan <jan-d.pietsch@live.de>
2025-05-01 15:00:35 +0200

    ui fix

--- src/components/QuestEditor.vue
+++ src/components/QuestEditor.vue
@@ -13,7 +13,7 @@ defineEmits(['submit']);
 </script>
 
 <template>
-  <div class="flex flex-col w-full max-w-sm gap-3">
+  <div class="flex flex-col h-dvw max-w-xl mx-auto justify-center items-center gap-3">
     <input v-model="title" type="text" placeholder="Title" class="input w-full" />
     <textarea v-model="description" class="textarea w-full" placeholder="Description"></textarea>
     <input v-model="questpoints" type="number" class="input validator w-full" required placeholder="Side Quest Points" min="1" title="Must be greater than 1" />
