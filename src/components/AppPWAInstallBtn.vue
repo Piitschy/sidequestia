9fe1bc2e53b8b83fd4343d22e79958efc838a374
Jan <jan-d.pietsch@live.de>
2025-05-25 00:50:36 +0200

    force ask for install

--- src/components/AppPWAInstallBtn.vue
+++ src/components/AppPWAInstallBtn.vue
@@ -1,20 +1,37 @@
 <script lang="ts" setup>
+import { useThemeStore } from '@/stores/theme';
 import { Icon } from '@iconify/vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+
+type InstallPromptEvent = Event & {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: string; platform: string }>;
+};
 
 const hidden = ref(true);
-const installPrompt = ref<Event | null>(null);
+const installPrompt = ref<InstallPromptEvent|null>(null);
 
 window.addEventListener("beforeinstallprompt", (event) => {
-  installPrompt.value = event;
+  installPrompt.value = event as InstallPromptEvent;
   hidden.value = false;
+});
+
+const themeStore = useThemeStore();
+
+onMounted(() => {
+  setTimeout(() => {
+    if (!themeStore.askedForInstall && installPrompt.value) {
+      installPrompt.value?.prompt()
+      themeStore.askedForInstall = true;
+    }
+  }, 3000)
 });
 </script>
 
 <template>
   <button
     class="btn btn-ghost btn-square"
-    @click="(installPrompt as any)?.prompt()"
+    @click="installPrompt?.prompt()"
     :hidden
   >
     <Icon icon="ic:twotone-install-mobile" />
