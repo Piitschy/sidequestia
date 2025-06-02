2a0052bfa42f2316ebac64aca909ae4b947593cf
Jan <jan-d.pietsch@live.de>
2025-06-02 10:51:47 +0200

    unmount pb_public path

--- server/main.go
+++ server/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		// serves static files from the provided public dir (if exists)
-		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+		// se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 		g := se.Router.Group("/api/v1")
 		g.Bind(apis.RequireAuth())
 		g.POST("/accept-quest", AcceptQuestHandler)
